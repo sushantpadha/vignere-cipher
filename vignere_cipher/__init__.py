7550f6122353e252505b4c7fd97958a8b1caf976
sushant <sushant.padha@gmail.com>
2021-04-15 12:24:36 +0530

    refactor ; added encrypt decrypt driver code

--- vignere_cipher/__init__.py
+++ vignere_cipher/__init__.py
@@ -1,45 +1,108 @@
-class Vignere:
-    def __init__(self, msg: str, key: str):
-        self.msg = msg
-        self.key = self.get_key(len(msg), key)
+import argparse
 
-    @staticmethod
-    def get_table():
-        pass
+alphabet = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
 
-    @staticmethod
-    def get_key(length: int, key: str):
-        '''Return key with length same as msg
 
-        Args:
-            length (int): length of msg
-            key (str): original key
+def normalize(s: str):
+    '''Remove non-alphabetic characters
 
-        Returns:
-            str: expanded key
-        '''
-        key_len = len(key)
-        # repeat
-        if key_len < length:
-            key *= (length // key_len) + 1
-        # slice
-        return key[:length]
+    Args:
+        s (str): string to normalize
 
-    def encrypt():
-        pass
+    Returns:
+        str: new string with only alphabetic characters
+    '''
+    r = ""
+    for c in s:
+        if c not in alphabet:
+            continue
+        r += c
+    return r
 
-    def decrypt():
-        pass
+
+def get_key(text: str, key: str):
+    '''Return key with length same as text
+
+    Args:
+        text (str): text to expand key for
+        key (str): original key
+
+    Returns:
+        str: expanded key
+    '''
+    length = len(text)
+    key_len = len(key)
+    # repeat
+    if key_len < length:
+        key *= (length // key_len) + 1
+    # slice
+    return key[:length]
+
+
+def encrypt(plaintext: str, key: str):
+    encrypted = ""
+    plaintext = plaintext.lower()
+    for i in range(len(plaintext)):
+        t = plaintext[i]
+        k = key[i]
+        encrypted += alphabet[
+            (alphabet.index(t) + alphabet.index(k)) % 26
+        ]
+    return encrypted
+
+
+def decrypt(encrypted: str, key: str):
+    plaintext = ""
+    for i in range(len(encrypted)):
+        e = encrypted[i]
+        k = key[i]
+        plaintext += alphabet[
+            (alphabet.index(e) - alphabet.index(k)) % 26
+        ]
+    return plaintext
 
 
 def main():
-    pass
+    namespace = parse_args()
+    parsed_vars = vars(namespace)
+    text = parsed_vars['text']
+    key = parsed_vars['key']
+    e = parsed_vars['e']
+    d = parsed_vars['d']
+
+    text = normalize(text).lower()
+    key = get_key(text, normalize(key).lower())
+
+    if e:
+        r = encrypt(text, key)
+    else:
+        r = decrypt(text, key)
+    print(r)
 
 
 def parse_args():
-    pass
+    parser = argparse.ArgumentParser(
+        description="encrypt and decipher messages with vignere cipher",
+        epilog="\x1b[3m“Sometimes it’s the people no one " +
+        "imagines anything of who do the things " +
+        "that no one can imagine.”\x1b[0m (Imitation Game, 2014)"
+    )
+    parser.add_argument('text', action='store',
+                        help="text message to decrypt or encrypt " +
+                        "(all non-alphabetic characters will be removed)")
+    parser.add_argument('-k', action='store', dest='key', required=True,
+                        help="key to use to en/decrypt text " +
+                        "(all non-alphabetic characters will be removed)")
+    action = parser.add_mutually_exclusive_group()
+    action.add_argument('-e', action='store_true', dest='e',
+                        help="encrypt text using key")
+    action.add_argument('-d', action='store_true', dest='d',
+                        help="decrypt text using key")
+    return parser.parse_args()
 
 
-if __name__=='__main__':
-    pass
-
+if __name__ == '__main__':
+    main()
