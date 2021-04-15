3d2650b5f8ebf1bc954663ed794a39fe3300ad12
sushant <sushant.padha@gmail.com>
2021-04-15 09:18:27 +0530

    Initial Commit

--- vignere_cipher/__init__.py
+++ vignere_cipher/__init__.py
@@ -1,20 +1,31 @@
 def get_table():
     pass
 
+
 def get_key():
     pass
+
 
 def vignere_cipher():
     pass
 
+
 def encrypt():
     pass
+
 
 def decrypt():
     pass
 
+
 def main():
     pass
 
+
 def parse_args():
     pass
+
+
+if __name__=='__main__':
+    pass
+
