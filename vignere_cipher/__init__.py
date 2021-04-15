21780e16090f55061aa6a06644a6d2bd318d42f8
sushant <sushant.padha@gmail.com>
2021-04-15 10:35:29 +0530

    grouped funcs into Vignere class ; added code

--- vignere_cipher/__init__.py
+++ vignere_cipher/__init__.py
@@ -1,21 +1,35 @@
-def get_table():
-    pass
+class Vignere:
+    def __init__(self, msg: str, key: str):
+        self.msg = msg
+        self.key = self.get_key(len(msg), key)
 
+    @staticmethod
+    def get_table():
+        pass
 
-def get_key():
-    pass
+    @staticmethod
+    def get_key(length: int, key: str):
+        '''Return key with length same as msg
 
+        Args:
+            length (int): length of msg
+            key (str): original key
 
-def vignere_cipher():
-    pass
+        Returns:
+            str: expanded key
+        '''
+        key_len = len(key)
+        # repeat
+        if key_len < length:
+            key *= (length // key_len) + 1
+        # slice
+        return key[:length]
 
+    def encrypt():
+        pass
 
-def encrypt():
-    pass
-
-
-def decrypt():
-    pass
+    def decrypt():
+        pass
 
 
 def main():
